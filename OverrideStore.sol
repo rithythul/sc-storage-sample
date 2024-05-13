f29f25830668a930e07069ebb0b6435b0ec1b48d
Rithy Thul <rithythul@gmail.com>
2024-05-13 20:30:52 +0700

    Update and rename Add100Store.sol to OverrideStore.sol

--- OverrideStore.sol
+++ OverrideStore.sol
@@ -3,9 +3,9 @@ pragma solidity ^0.8.18;
 
 import {SimpleStorage} from "./SimpleStorage.sol";
 
-contract AddFiveStore is SimpleStorage {
+contract OverrideStore is SimpleStorage {
 
     function store(uint256 _newNumber) public override {
-    myFavoritNumber = _newNumber + 100; 
+    myFavoritNumber = _newNumber + 10; 
     }
 }
